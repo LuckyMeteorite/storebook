99902a9743848f26f7219cdc52f73c8ada8a7863
LuckyMeteorite <mastermind5000@gmail.com>
2018-09-25 19:34:51 +0300

    Entity annotaatio ja application propertiesseihin lisättiin juttui

--- src/main/java/bookstore/beans/Book.java
+++ src/main/java/bookstore/beans/Book.java
@@ -1,7 +1,16 @@
 package bookstore.beans;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Book {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
 	private String title;
 	private String writer;
 	private String isbn;
