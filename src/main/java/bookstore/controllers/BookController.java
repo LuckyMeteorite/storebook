a5344f2cf7566699040d9113bebfd62b118a4845
LuckyMeteorite <mastermind5000@gmail.com>
2018-10-02 19:29:09 +0300

    addbook valmis

--- src/main/java/bookstore/controllers/BookController.java
+++ src/main/java/bookstore/controllers/BookController.java
@@ -4,9 +4,13 @@ import java.util.ArrayList;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import bookstore.model.Book;
 import bookstore.model.BookRepository;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -40,8 +44,15 @@ public class BookController {
 
 	@GetMapping("addbook")
 	public String addBook(@RequestParam(required = false) String book, Model model) {
-		model.addAttribute("bookshelf", bookshelf.findAll());
+		model.addAttribute("book", new Book());
 		return "addbook";
 	}
+	
+	// Save new book
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String save(Book book){
+        bookshelf.save(book);
+        return "redirect:bookshelf";
+    }    
 
 }
